46a6b884785390eaa85d8a0c9135281a2f26a900
Ian Clarke <github@ian.33mail.com>
2019-01-05 18:52:50 -0600

    misc improvements

--- conf.py
+++ conf.py
@@ -136,7 +136,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Kweb.tex', u'Kweb: The Contrarian Web Framework',
+    (master_doc, 'Kweb.tex', u'Kweb: The Easier Way to Website',
      u'Ian Clarke', 'manual'),
 ]
 
@@ -146,7 +146,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'kweb', u'The Contrarian Web Framework',
+    (master_doc, 'kweb', u'The Easier Way to Website',
      [author], 1)
 ]
 
@@ -157,8 +157,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Kweb', u'The Contrarian Web Framework',
-     author, 'Kweb', 'The Contrarian Web Framework',
+    (master_doc, 'Kweb', u'The Easier Way to Website',
+     author, 'Kweb', 'The Easier Way to Website',
      'Miscellaneous'),
 ]
 
--- index.rst
+++ index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Kweb: The Contrarian Web Framework
+Kweb: The Easier Way to Website
 ===================================
 
 Create beautiful, efficient, and powerful websites in Kotlin.
